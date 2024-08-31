c1026b8ab0a9b0fdcd7183686a8ab0f7411c6be2
Joshua Richardson <josh@cursedtale.com>
2024-08-30 23:19:05 -0700

    feat(home-page): implement home page

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const postcssJitProps = require('postcss-jit-props')
-const OpenProps = require('open-props')
-const autoprefixer = require('autoprefixer')
-
-module.exports = {
-  plugins: [autoprefixer, postcssJitProps(OpenProps)],
-}
