6e2733833dd04a13db2d4e95eb6c6f9858e4a16e
Joshua Richardson <josh@cursedtale.com>
2024-08-31 00:43:54 -0700

    feat(projects-page): Implement

---
+++ src/data/projects.ts
@@ -0,0 +1,92 @@
+export const projects = [
+  {
+    title: 'Barbie Selfie Generator',
+    description:
+      'An interactive selfie generator that allows users to create a movie poster like they were a cast member of the movie.',
+    image: '/barbie.jpg',
+    technologies: ['Vue2', 'Nuxt2', 'PhotoRoom API'],
+    liveUrl: 'https://www.barbieselfie.ai/',
+  },
+  {
+    title: 'Oppenheimer',
+    description:
+      'A multi-phase project to create buzz for the Oppenheimer movie release.',
+    image: '/oppenheimer.jpg',
+    technologies: ['Vue2', 'Nuxt2', 'Three.js'],
+    liveUrl:
+      'https://web.archive.org/web/20240510130935/https://www.oppenheimermovie.com/',
+  },
+  {
+    title: 'Expend4bles Poster Generator',
+    description:
+      'An interactive poster generator that allows users to create a movie poster like they were a cast member of the movie.',
+    image: '/expend4bles.jpg',
+    technologies: ['Vue2', 'Nuxt2', 'PhotoRoom API'],
+    liveUrl: 'https://join.expendables.movie/',
+  },
+  {
+    title: 'The Watchers',
+    description:
+      "A web experience for The Watchers movie release, featuring fleeting clips from the film as well as a day/night transition aligning with the user's region.",
+    image: '/watchers.jpg',
+    technologies: ['Vue2', 'Nuxt2'],
+    liveUrl: 'https://www.areyouwatching.us/',
+  },
+  {
+    title: 'Wild Rift Anniversary Site',
+    description:
+      'An interactive multi-phase project for the third anniversary of Wild Rift featuring a noir-inspired puzzle game with prizes provided in conjunction with Coca-Cola, as well as allowing users to share memories they had with the game.',
+    image: '/wr.jpg',
+    technologies: ['Vue2', 'Nuxt2', 'Node.js'],
+    liveUrl: 'https://anniversary.wildrift.leagueoflegends.com/en-us/',
+  },
+  {
+    title: 'TFT - Set 10: Remix Rumble',
+    description:
+      'A web and in-game client experience celebrating the launch of TFT Set 10.',
+    image: '/tft-s10.webp',
+    technologies: ['Vue2', 'Nuxt2', 'League Client API'],
+    liveUrl:
+      'https://remix-rumble.teamfighttactics.leagueoflegends.com/en-us/remix/',
+  },
+  {
+    title: 'Bond Website',
+    description:
+      'A refreshed company website for Bond Creative to showcase the world-class entertainment pieces we worked on.',
+    image: '/bond.jpg',
+    technologies: ['Vue3', 'Nuxt3', 'React', 'Payload CMS'],
+    liveUrl: 'https://www.wearebond.com',
+  },
+  {
+    title: 'Skydance Media Website',
+    description:
+      'A marketing site showcasing the projects that Skydance Media was involved in.',
+    image: '/skydance.png',
+    technologies: ['Vue2', 'Wordpress'],
+    liveUrl: 'https://skydance.com/',
+  },
+  {
+    title: 'Accelerise Media Website',
+    description:
+      'A marketing site showcasing the projects that Accelerise Media was involved in.',
+    image: '/accelerise.jpg',
+    technologies: ['React', 'Gatsby', 'Framer Motion'],
+    liveUrl: 'https://accelarisemedia.com/',
+  },
+  {
+    title: 'Grepl',
+    description: 'A boggle clone built with Next.js, Clerk, and SQLite.',
+    image: '/grepl.png',
+    technologies: ['React', 'Next.js', 'SQLite', 'Clerk'],
+    liveUrl: 'https://grepl.cursedtale.com',
+    githubUrl: 'https://github.com/joshatbond/grepl',
+  },
+  {
+    title: 'Portfolio',
+    description: 'This very website',
+    image: '/portfolio.png',
+    technologies: ['Astro.js', 'React', 'Shadcn', 'React-Three-Fiber'],
+    liveUrl: 'https://portfolio.cursedtale.com',
+    githubUrl: 'https://github.com/joshatbond/portfolio-static',
+  },
+]
